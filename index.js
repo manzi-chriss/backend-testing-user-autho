dacd4fe4d49ddf61eb5e3369f91873362c58badc
manzi-chriss <manzichrss6@gmail.com>
2024-08-05 18:27:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express= require('express');
+const {connectDB}= require('./model/DBconnection')
+const  cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+require('dotenv').config();
+
+
+
+connectDB();
+const app=express();
+const port=3000;
+
+app.use(express.json());
+app.use(cors());
+const userRoutes = require('./controllers/routes/User');
+const accountRoutes = require('./controllers/routes/account')
+const loginRoute = require('./controllers/routes/Login')
+
+app.use('/api/users',userRoutes);
+app.use('/api/account',accountRoutes)
+app.use('/api/login',loginRoute)
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.listen(port, () => {
+    // console.log(`Server running on port http://localhost:${port}`);
+});
